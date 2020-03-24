3264b4b9e9bf0756d80c75a25faeb8c56bde5efc
unknown <you@example.com>
2020-03-24 09:15:55 +0800

    “提交信息”

---
+++ Chapter6/Serv/6asyn/echo/Main.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+class MainClass
+{
+	public static void Main(string[] args)
+	{
+		Console.WriteLine("Hello World!");
+		Serv serv = new Serv();
+		serv.Start("127.0.0.1", 1234);
+
+
+         
+        
+
+
+
+        while (true)
+		{
+			string str = Console.ReadLine();
+			switch (str)
+			{
+			case "quit":
+				return;
+			}
+		}
+
+
+
+
+
+
+	}
+}
